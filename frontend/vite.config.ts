939f896734367bf4c307ee5cc681a9c2aa2a7318
Ji Hua <michaelhua1993@foxmail.com>
2026-04-10 18:40:43 -0400

    Feature: add k8s and upgrade docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,5 +12,16 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 8765,
     strictPort: true,
+    proxy: {
+      '/api': {
+        target: 'http://backend:8764',
+        changeOrigin: true,
+      },
+      '/ws': {
+        target: 'ws://backend:8764',
+        ws: true,
+        changeOrigin: true,
+      },
+    },
   },
 })
